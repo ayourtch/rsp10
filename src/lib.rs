68c98ed0a53bd957b899f9a89876b500778cc25a
Andrew Yourtchenko <ayourtch@gmail.com>
2023-10-05 18:54:23 +0200

    fix the html object type

--- src/lib.rs
+++ src/lib.rs
@@ -241,7 +241,7 @@ macro_rules! rsp10_text_escape_backtick {
 #[macro_export]
 macro_rules! rsp10_nested_text {
     ( $gd: ident, $parent: ident, $idx: expr, $elt: ident, $rinfo: ident, $modified: ident) => {
-        let mut $elt: std::rc::Rc<std::cell::RefCell<HtmlCheck>> =
+        let mut $elt: std::rc::Rc<std::cell::RefCell<HtmlText>> =
             std::rc::Rc::new(std::cell::RefCell::new(Default::default()));
         {
             let mut $elt = $elt.borrow_mut();
